9ecef5016a90b1dc7ccb0374dbea85302300d5ee
MinT <tnm1042002@gmail.com>
2024-12-09 22:34:29 +0700

    Update projectService.js

--- Backend/modules/project/services/projectService.js
+++ Backend/modules/project/services/projectService.js
@@ -9,3 +9,18 @@ exports.createProject = async (projectData) => {
 exports.getAllProjects = async () => {
     return await projectRepository.getAllProjects();
 };
+
+// Get a specific project by ID
+exports.getProjectById = async (projectId) => {
+    return await projectRepository.getProjectById(projectId);
+};
+
+// Update a project
+exports.updateProject = async (projectId, projectData) => {
+    return await projectRepository.updateProject(projectId, projectData);
+};
+
+// Delete a project
+exports.deleteProject = async (projectId) => {
+    return await projectRepository.deleteProject(projectId);
+};
