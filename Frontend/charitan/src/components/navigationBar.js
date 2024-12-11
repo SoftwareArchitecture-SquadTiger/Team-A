ffdbf80a3501b5193876638927977579393f0848
Ashton Pham <69978620+AshtonPh@users.noreply.github.com>
2024-12-11 23:30:45 +0700

    Create NavLink component that acts as links that can underline themselves when hovered

--- Frontend/charitan/src/components/navigationBar.js
+++ Frontend/charitan/src/components/navigationBar.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { AppBar, Toolbar, Typography, Box, Link, Grid } from '@mui/material';
 import CharitanLogo from './charitanLogo';
+import NavLink from './navLink';
 
 const NavigationBar = () => {
   return (
@@ -8,26 +9,15 @@ const NavigationBar = () => {
       <Toolbar>
         <Grid container alignItems="center">
             <Grid item xs={12} md={6} textAlign={{ xs: 'center', md: 'left' }}>
-                <CharitanLogo />
+                <CharitanLogo color="black"/>
             </Grid>
           <Grid item xs={12} md={6} textAlign={{ xs: 'center', md: 'right' }}>
             <Box sx={{ display: 'flex', flexDirection: 'row', gap: 2 }}>
-                <Link>Home</Link>
-                <Link href="/home" color="inherit" underline="none">
-                    <Typography variant="button" sx={{ color: 'black' }}>Home</Typography>
-                </Link>
-                <Link href="/discovery" color="inherit" underline="none">
-                    <Typography variant="button" sx={{ color: 'black' }}>Discovery</Typography>
-                </Link>
-                <Link href="/about-us" color="inherit" underline="none">
-                    <Typography variant="button" sx={{ color: 'black' }}>About Us</Typography>
-                </Link>
-                <Link href="/leaderboard" color="inherit" underline="none">
-                    <Typography variant="button" sx={{ color: 'black' }}>Leaderboard</Typography>
-                </Link>
-                <Link href="/history" color="inherit" underline="none">
-                    <Typography variant="button" sx={{ color: 'black' }}>History</Typography>
-                </Link>
+                <NavLink href="/home" color="black" size="1.2rem">Home</NavLink>
+                <NavLink href="/discovery" color="black" size="1.2rem">Discovery</NavLink>
+                <NavLink href="/about-us" color="black" size="1.2rem">About Us</NavLink>
+                <NavLink href="/leaderboard" color="black" size="1.2rem">Leaderboard</NavLink>
+                <NavLink href="/history" color="black" size="1.2rem">History</NavLink>
             </Box>
           </Grid>
         </Grid>
