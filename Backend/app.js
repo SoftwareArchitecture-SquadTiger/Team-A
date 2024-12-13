2fc49c7f8883d94e0fe600d1c9edf0128c4510d8
MinT <tnm1042002@gmail.com>
2024-12-13 23:46:47 +0700

    use Centralized routes

--- Backend/app.js
+++ Backend/app.js
@@ -3,11 +3,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./database/connection');
-const projectRoutes = require('./modules/project/routes/project');
-const categoryRoutes = require('./modules/project/routes/category');
-const imageRoutes = require('./modules/project/routes/image');
-const videoRoutes = require('./modules/project/routes/video');
-const emailRoutes = require('./modules/email/routes/emailRoutes');
+const routes = require('./routes'); // Centralized routes
+
 
 dotenv.config();
 
@@ -25,20 +22,8 @@ app.get('/', (req, res) => {
     res.send('Backend is running');
 });
 
-// Project routes
-app.use('/api/projects', projectRoutes);
-
-// Category routes
-app.use('/api/categories', categoryRoutes);
-
-// Image routes
-app.use('/api/images', imageRoutes);
-
-// Video routes
-app.use('/api/videos', videoRoutes);
-
-//Email routes
-app.use('/api/email', emailRoutes);
+//Centralized routes
+app.use('/api', routes);
 
 // Start server
 const PORT = process.env.PORT || 5000;
