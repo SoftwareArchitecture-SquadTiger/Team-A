3d2afcceab7bec008433d49b83b7ad640f69d109
Ashton Pham <69978620+AshtonPh@users.noreply.github.com>
2024-12-14 21:09:43 +0700

    Redirect the page to the homepage

--- Frontend/charitan/src/App.js
+++ Frontend/charitan/src/App.js
@@ -1,13 +1,17 @@
 import "./App.css";
 import Footer from "./components/footer";
 import NavigationBar from "./components/navigationBar";
+import DonationBanner from "./components/donationBanner";
 import RouteConfig from "./rooter/RouteConfig";
 import { BrowserRouter } from "react-router-dom";
+import charityImage from './assets/charity.png'; 
+
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
+      <DonationBanner imageSrc={charityImage} height="35vh" />
       <BrowserRouter>
         <main>
           <RouteConfig />
--- Frontend/charitan/src/rooter/RouteConfig.jsx
+++ Frontend/charitan/src/rooter/RouteConfig.jsx
@@ -4,14 +4,15 @@ import { CircularProgress } from '@mui/material';
 import SamplePage from '../pages/SamplePage';
 import SigninPage from '../pages/SigninPage';
 import RecoveryPage from '../pages/RecoveryPage';
+import HomePage from '../pages/HomePage';
 
 const RouteConfig = () => {
 
     return (
         <Suspense fallback={<CircularProgress className="w-8 h-8" />}>
             <Routes>
-                <Route path="/" element={< SamplePage />} />
-                <Route path="/home" element={< SamplePage />} />
+                <Route path="/" element={< HomePage />} />
+                <Route path="/home" element={< HomePage />} />
                 <Route path="/signin" element={<SigninPage />} />
                 <Route path="/signup" element={<SigninPage />} />
                 <Route path="/recovery" element={<RecoveryPage />} />
